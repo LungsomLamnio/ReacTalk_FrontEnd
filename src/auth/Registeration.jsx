989aba9939783193b0ca695f9621501617527146
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-06-10 18:12:05 +0530

    registeration page design done

--- src/auth/Registeration.jsx
+++ src/auth/Registeration.jsx
@@ -1,19 +1,39 @@
 import { Form, Button, Container, Row, Col, Card } from "react-bootstrap";
+import { useState } from "react";
 
 export default function Registeration() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData((prevValue) => {
+      return { ...prevValue, [event.target.name]: event.target.value };
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
   return (
     <Container className="mt-5">
       <Row className="justify-content-center">
         <Col md={6}>
           <Card className="p-4 shadow-sm">
             <h3 className="text-center mb-4">User Registeration</h3>
-            <Form>
+            <Form onSubmit={handleSubmit}>
               <Form.Group className="mb-3" controlId="formName">
                 <Form.Label>Full Name</Form.Label>
                 <Form.Control
                   type="text"
                   name="name"
                   placeholder="Enter your full name"
+                  onChange={handleChange}
+                  value={formData.name}
                   required
                 />
               </Form.Group>
@@ -23,6 +43,8 @@ export default function Registeration() {
                   type="email"
                   name="email"
                   placeholder="Enter your email"
+                  onChange={handleChange}
+                  value={formData.email}
                   required
                 />
               </Form.Group>
@@ -32,6 +54,8 @@ export default function Registeration() {
                   type="password"
                   name="password"
                   placeholder="Enter your password"
+                  onChange={handleChange}
+                  value={formData.password}
                   required
                 />
               </Form.Group>
@@ -41,6 +65,8 @@ export default function Registeration() {
                   type="password"
                   name="confirmPassword"
                   placeholder="Confirm your password"
+                  onChange={handleChange}
+                  value={formData.confirmPassword}
                   required
                 />
               </Form.Group>
