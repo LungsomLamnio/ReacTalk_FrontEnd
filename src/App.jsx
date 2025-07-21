63d301b2fd8f4451a4a9e12e66297fdf7863825c
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-07-21 21:54:04 +0530

    sample chat page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import UserLogin from "./pages/UserLogin";
 import UserSignup from "./pages/UserSignUp";
+import ChatPage from "./pages/ChatPage";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/user-login" element={<UserLogin />} />
       <Route path="/user-signup" element={<UserSignup />} />
+      <Route path="/chat" element={<ChatPage />} />
     </Routes>
   );
 }
---
+++ src/pages/ChatPage.jsx
@@ -0,0 +1,7 @@
+export default function ChatPage() {
+  return (
+    <div>
+      <h2>Chat Page</h2>
+    </div>
+  );
+}
